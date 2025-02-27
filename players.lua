1a6c45e04d6d716b170c9f2019f0c1154aa21114
k-Knight <28106261+k-Knight@users.noreply.github.com>
2025-02-27 15:07:28 +0200

    trying to fix cheat

--- players.lua
+++ players.lua
@@ -1,4 +1,4 @@
-local replicationInterface = shared.require(ReplicationInterface)
+local replicationInterface = shared.require(script:WaitForChild("ReplicationInterface"))
 local playerService = game:GetService("Players")
 local teamService = game:GetService("Teams")
 
